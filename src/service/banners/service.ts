434c03b2f04a50ab3b3675ddc73870aa67a1830d
dan120929 <dan.salgado58d@gmail.com>
2024-08-29 07:51:57 -0600

    Actualizar service.ts

--- src/service/banners/service.ts
+++ src/service/banners/service.ts
@@ -15,22 +15,42 @@ export  async function GetBannersLocation(location:string):Promise<Response<Bann
     error: `${err}`
   };;
 }}
-export  async function PostBannersLocation(banner:BannerPost):Promise<Response<Banner>>{
+export  async function PostBannersLocation(banner:BannerPost){
   
   try{
-    const formData = new FormData();
-    Object.keys(banner).forEach(key => {if(banner[key as keyof BannerPost] != null)formData.append(key, banner[key as keyof BannerPost]! )})
-    console.log(banner.AssetFile?.size)
-  return await request<Banner>({
-    method:"POST",
-    endpoint:`/api/Assets/upload-asset`,
-    headers:{
-      "Content-Type": `multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW`,},
-    formData: formData
-  })
+
+        const formData = new FormData();
+        formData.append('AssetName', banner.AssetName);
+        formData.append('AssetDescription', banner.AssetDescription);
+        formData.append('Location', banner.Location);
+        formData.append('AssetFile', banner.AssetFile!);
+
+        try {
+            const response = await fetch('https://localhost:5015/api/Assets/upload-asset', {
+              method:"POST",
+              headers: {
+                "Content-Type": `multipart/form-data;`,
+                
+              },
+              body: formData
+            });
+        } catch (error) {
+            console.error('Error uploading asset:', error);
+           
+        }
+  
 }catch(err){
   return  {
     status: 500,
     error: `${err}`
   };;
 }}
+// const formData = new FormData();
+//     Object.keys(banner).forEach(key => {if(banner[key as keyof BannerPost] != null)formData.append(key, banner[key as keyof BannerPost]! )})
+//     console.log(banner.AssetFile?.size)
+//   return await request<Banner>({
+//     method:"POST",
+//     endpoint:`/api/Assets/upload-asset`,
+//     headers:{
+//       "Content-Type": `multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW`,},
+//     formData: formData
