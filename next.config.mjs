7a1220c8e21d1dfaf0b8e6045756544573a2373e
Daniel Perez <danielps@setenal.mx>
2024-08-30 10:59:26 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: '/cms',
+    images: {
+        // Asegúrate de que los dominios permitidos sean correctos si estás cargando imágenes de un dominio externo
+        domains: ['64.23.178.44'], // Cambia esto según sea necesario
+      },
 };
 
 export default nextConfig;
