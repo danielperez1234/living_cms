e92ae63f1a83cc73f1ea2564c2b746b098bd376e
danielperez1234 <95051521+danielperez1234@users.noreply.github.com>
2025-01-21 12:09:45 -0600

    Update paginado.tsx

--- src/components/common/paginado.tsx
+++ src/components/common/paginado.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 
-const SimplePagination: React.FC = ({onChange}:{onChange?:()=>void}) => {
+const SimplePagination: React.FC = ({onChange}:{onChange?:(page:number)=>void}) => {
   const handlePageChange = (event: React.ChangeEvent<unknown>, page: number) => {
+    if(onChange){
+      onChange(page);
+    }
   };
 
   return (
